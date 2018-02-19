3e6419776959eefdbc748fb9a3df5204c4e55006
Paul Pinho <pinhop07@gmail.com>
2018-02-18 22:14:49 -0500

    initial commit

--- bamazon.sql
+++ bamazon.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE Bamazon;
+CREATE DATABASE bamazon;
 
 USE bamazon;
 
@@ -11,72 +11,72 @@ StockQuantity int NOT NULL);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 12345,
-'Duvet Cover',
-'Home',
-89.99,
+'Spider-Man: Homecoming',
+'Movie',
+19.99,
 25);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 67891,
-'Mens Zip-up Hoodie',
-'Mens Clothing',
-29.99,
+'Guardians of the Galaxy: Awesome Mix Vol. 1',
+'Music',
+9.99,
 25);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 23456,
-'Glass Catch-all Dish',
-'Home',
-15.99,
+'Guardians of the Galaxy: Awesome Mix Vol. 1',
+'Music',
+9.99,
 10);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 78912,
-'Record Player',
-'Electronics',
-10.99,
+'Wolverine: Origins No.28 Cover: Wolverine',
+'Posters',
+15.99,
 8);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 34567,
-'High Rise Skinny Jeans - Black',
-'Womens Clothing',
-59.99,
+'Maximum Carnage',
+'Video Games',
+29.99,
 30);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 89123,
-'Womens Bruce Springsteen Baseball T-shirt',
-'Womens Clothing',
-25.99,
+'X-Men: Clone Wars',
+'Video Games',
+29.99,
 8);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 45678,
-'Mens Knit Beanie - Blue',
-'Mens Clothing',
-21.99,
+'Civil War',
+'Comics',
+10.99,
 15);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 91234,
-'Striped Jersey Cotton Sheets - Queen',
-'Home',
-49.99,
+'The Night Gwen Stacy Died',
+'Comics',
+10.99,
 10);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 56789,
-'Spiked Dog Leash',
-'Pets',
-25.99,
+'Dark Phoenix Saga',
+'Comics',
+10.99,
 10);
 
 INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
 11234,
-'Shark Kitty Bed',
-'Pets',
-59.99,
+'Infinity Gauntlet',
+'Comics',
+10.99,
 5);
 
 USE bamazon;
@@ -89,32 +89,32 @@ TotalSales DECIMAL(11,2) NOT NULL);
 
 
 INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Mens Clothing',
+'Comic Books',
 10000,
 0);
 
 INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Pets',
+'Movies',
 10000,
 0);
 
 INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Home',
+'Sound Tracks',
 20000,
 0);
 
 INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Shoes',
+'Video Games',
 15000,
 0);
 
 INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Electronics',
+'Toys',
 50000,
 0);
 
 INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Womens Clothing',
+'Posters',
 25000,
 0);
 
